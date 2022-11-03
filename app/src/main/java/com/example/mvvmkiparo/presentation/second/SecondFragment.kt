ea0d9cc14dea116ba71b36eb19b3475d200c2ee1
TorisonRa <torisonra@ya.ru>
2022-11-03 21:47:24 +0900

    работа с толбоксом

--- app/src/main/java/com/example/mvvmkiparo/presentation/second/SecondFragment.kt
+++ app/src/main/java/com/example/mvvmkiparo/presentation/second/SecondFragment.kt
@@ -2,16 +2,15 @@ package com.example.mvvmkiparo.presentation.second
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.mvvmkiparo.R
 import com.example.mvvmkiparo.databinding.FragmentSecondBinding
-import com.google.android.material.navigation.NavigationBarView
 
 class SecondFragment : Fragment() {
 
@@ -47,15 +46,22 @@ class SecondFragment : Fragment() {
                 binding.topAppBar.setNavigationIcon(R.drawable.ic_down_circle)
             } else {
                 binding.navigationRail.isVisible = true
-                binding.topAppBar.setNavigationIcon(R.drawable.ic_left)
+                binding.topAppBar.setNavigationIcon(R.drawable.ic_up_circle)
             }
         }
 
         binding.navigationRail.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.stopwatch -> {
-                    // binding.topAppBar.isVisible = true
-                    binding.navigationRail.isVisible = false
+                R.id.watch -> {
+                    true
+                }
+                R.id.document -> {
+                    true
+                }
+                R.id.edit -> {
+                    true
+                }
+                R.id.setting -> {
                     true
                 }
                 else -> false
